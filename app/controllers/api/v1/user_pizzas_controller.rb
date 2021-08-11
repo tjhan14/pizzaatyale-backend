e730b9475354a176565996d7323f5bc4f6300c4d
Taek Jin Han <tj.han@yale.edu>
2021-08-11 17:13:45 -0400

    working on delete function

--- app/controllers/api/v1/user_pizzas_controller.rb
+++ app/controllers/api/v1/user_pizzas_controller.rb
@@ -6,4 +6,25 @@ class Api::V1::UserPizzasController < ApplicationController
     ##render json: user, include: :pizzas
     # end
 
+    def index
+        user_pizzas = UserPizza.all
+        render json: user_pizzas
+    end
+    
+    def create
+        user_pizza = UserPizza.create(rating: user_pizza_params[:rating], comment: user_pizza_params[:comment], pizza_id: user_pizza_params[:pizza_id], user_id: User.first.id)
+        render json: user_pizza 
+    end
+
+    def destroy
+        user_pizza = UserPizza.delete(rating: user_pizza_params[:rating], comment: user_pizza_params[:comment], pizza_id: user_pizza_params[:pizza_id], user_id: User.first.id)
+        render json: user_pizza
+    end
+
+    private
+
+    def user_pizza_params
+        params.require(:user_pizza).permit(:rating, :comment, :pizza_id, :user_id) 
+    end
+
 end
